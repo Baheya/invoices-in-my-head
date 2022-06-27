3a69dce214e6db42e78033e69d5d8c4e59ab156e
Baheya <khalifa.baheya@gmail.com>
2022-06-27 10:47:00 +0200

    added dark mode support to invoice item component

--- app/components/InvoiceItem/InvoiceStatus/InvoiceStatus.jsx
+++ app/components/InvoiceItem/InvoiceStatus/InvoiceStatus.jsx
@@ -6,16 +6,18 @@ export function links() {
 
 const STYLES = {
   pending: {
-    color: 'hsla(34, 100%, 50%, 1)',
-    backgroundColor: 'hsla(34, 100%, 50%, 0.06)',
+    '--color': 'hsla(34, 100%, 50%, 1)',
+    '--backgroundColor': 'hsla(34, 100%, 50%, 0.06)',
   },
   paid: {
-    color: 'hsla(160, 67%, 52%, 1)',
-    backgroundColor: 'hsla(160, 67%, 52%, 0.06)',
+    '--color': 'hsla(160, 67%, 52%, 1)',
+    '--backgroundColor': 'hsla(160, 67%, 52%, 0.06)',
   },
   draft: {
-    color: 'hsla(231, 75%, 93%, 1)',
-    backgroundColor: 'hsla(231, 75%, 93%, 0.06)',
+    '--color': 'hsla(231, 75%, 93%, 1)',
+    '--backgroundColor': 'hsla(231, 75%, 93%, 0.06)',
+    '--colorDark': 'hsla(231, 20%, 27%, 1)',
+    '--backgroundColorDark': 'hsla(231, 20%, 27%, 0.06)',
   },
 };
 

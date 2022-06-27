96855a665edcd5b0a75fc889b23089f803f2a60f
Baheya <khalifa.baheya@gmail.com>
2022-06-27 13:19:32 +0200

    added dialog component

---
+++ app/components/ModalDialog/ModalDialog.jsx
@@ -0,0 +1,67 @@
+import { useRef } from 'react';
+import { useOverlayTriggerState } from '@react-stately/overlays';
+import {
+  useOverlay,
+  usePreventScroll,
+  useModal,
+  OverlayProvider,
+  OverlayContainer,
+} from '@react-aria/overlays';
+import { useDialog } from '@react-aria/dialog';
+import { FocusScope } from '@react-aria/focus';
+import { useButton } from '@react-aria/button';
+
+import styles from './ModalDialog.css';
+
+export function links() {
+  return [{ rel: 'stylesheet', href: styles }];
+}
+
+export function ModalDialog(props) {
+  let { title, children } = props;
+
+  let ref = useRef();
+  let { overlayProps, underlayProps } = useOverlay(props, ref);
+
+  usePreventScroll();
+  let { modalProps } = useModal();
+
+  let { dialogProps, titleProps } = useDialog(props, ref);
+
+  return (
+    <div
+      style={{
+        position: 'fixed',
+        zIndex: 100,
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        background: 'rgba(0, 0, 0, 0.5)',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+      {...underlayProps}
+    >
+      <FocusScope contain restoreFocus autoFocus>
+        <div
+          {...overlayProps}
+          {...dialogProps}
+          {...modalProps}
+          ref={ref}
+          style={{
+            background: 'white',
+            color: 'black',
+            padding: 30,
+          }}
+        >
+          <h3 {...titleProps} style={{ marginTop: 0 }}>
+            {title}
+          </h3>
+          {children}
+        </div>
+      </FocusScope>
+    </div>
+  );
+}
